6562f3ccb8dcd7c49d54e5a5b4ec93da881a06a5
chiumentocam <46933927+chiumentocam@users.noreply.github.com>
2019-04-23 03:09:52 -0400

    apr23-0309

--- p5/js/main.js
+++ p5/js/main.js
@@ -1,3 +1,149 @@
 $(document).ready(function() {
-	
+	$('#circle1R').click(function() {
+		$('#circle1').css('z-index', '4');
+	});
+	$('#circle2R').click(function() {
+		$('#circle2').css('z-index', '4');
+	});
+	$('#circle3R').click(function() {
+		$('#circle3').css('z-index', '4');
+	});
+	$('#circle4R').click(function() {
+		$('#circle4').css('z-index', '4');
+	});
+	$('#circle5R').click(function() {
+		$('#circle5').css('z-index', '4');
+	});
+	$('#circle6R').click(function() {
+		$('#circle6').css('z-index', '4');
+	});
+	$('#circle7R').click(function() {
+		$('#circle7').css('z-index', '4');
+	});
+	$('#circle8R').click(function() {
+		$('#circle8').css('z-index', '4');
+	});
+	$('#circle9R').click(function() {
+		$('#circle9').css('z-index', '4');
+	});
+	$('#circle10R').click(function() {
+		$('#circle10').css('z-index', '4');
+	});
+	$('#circle11R').click(function() {
+		$('#circle11').css('z-index', '4');
+	});
+	$('#circle12R').click(function() {
+		$('#circle12').css('z-index', '4');
+	});
+	$('#circle13R').click(function() {
+		$('#circle13').css('z-index', '4');
+	});
+	$('#circle14R').click(function() {
+		$('#circle14').css('z-index', '4');
+	});
+	$('#circle15R').click(function() {
+		$('#circle15').css('z-index', '4');
+	});
+	$('#circle16R').click(function() {
+		$('#circle16').css('z-index', '4');
+	});
+	$('#circle17R').click(function() {
+		$('#circle17').css('z-index', '4');
+	});
+	$('#circle18R').click(function() {
+		$('#circle18').css('z-index', '4');
+	});
+	$('#circle19R').click(function() {
+		$('#circle19').css('z-index', '4');
+	});
+	$('#circle20R').click(function() {
+		$('#circle20').css('z-index', '4');
+	});
+	$('#circle21R').click(function() {
+		$('#circle21').css('z-index', '4');
+	});
+	$('#circle22R').click(function() {
+		$('#circle22').css('z-index', '4');
+	});
+	$('#circle23R').click(function() {
+		$('#circle23').css('z-index', '4');
+	});
+	$('#circle24R').click(function() {
+		$('#circle24').css('z-index', '4');
+	});
+	$('#circle25R').click(function() {
+		$('#circle25').css('z-index', '4');
+	});
+	$('#circle26R').click(function() {
+		$('#circle26').css('z-index', '4');
+	});
+	$('#circle27R').click(function() {
+		$('#circle27').css('z-index', '4');
+	});
+	$('#circle28R').click(function() {
+		$('#circle28').css('z-index', '4');
+	});
+	$('#circle29R').click(function() {
+		$('#circle29').css('z-index', '4');
+	});
+	$('#circle30R').click(function() {
+		$('#circle30').css('z-index', '4');
+	});
+	$('#circle31R').click(function() {
+		$('#circle31').css('z-index', '4');
+	});
+	$('#circle32R').click(function() {
+		$('#circle32').css('z-index', '4');
+	});
+	$('#circle33R').click(function() {
+		$('#circle33').css('z-index', '4');
+	});
+	$('#circle1').click(function() {
+		$('#circle1').css('z-index', '4');
+	});
+	$('#circle2').click(function() {
+		$('#circle2').css('z-index', '4');
+	});
+	$('#circle3').click(function() {
+		$('#circle3').css('z-index', '4');
+	});
+	$('#circle4').click(function() {
+		$('#circle4').css('z-index', '4');
+	});
+	$('#circle5').click(function() {
+		$('#circle5').css('z-index', '4');
+	});
+	$('#circle6').click(function() {
+		$('#circle6').css('z-index', '4');
+	});
+	$('#circle7').click(function() {
+		$('#circle7').css('z-index', '4');
+	});
+	$('#circle8').click(function() {
+		$('#circle8').css('z-index', '4');
+	});
+	$('#circle9').click(function() {
+		$('#circle9').css('z-index', '4');
+	});
+	$('#circle10').click(function() {
+		$('#circle10').css('z-index', '4');
+	});
+	$('#circle11').click(function() {
+		$('#circle11').css('z-index', '4');
+	});
+	$('#circle12').click(function() {
+		$('#circle12').css('z-index', '4');
+	});
+	$('#circle13').click(function() {
+		$('#circle13').css('z-index', '4');
+	});
+	$('#circle14').click(function() {
+		$('#circle14').css('z-index', '4');
+	});
+	$('#circle15').click(function() {
+		$('#circle15').css('z-index', '4');
+	});
+	$('#circle21').click(function() {
+		$('#circle21').css('z-index', '4');
+	});
 });
